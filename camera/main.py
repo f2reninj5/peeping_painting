7588845b640a6dd37a58f377d4e3f68cb0ec61e6
Oscar Ryley <oscar.ryley@gmail.com>
2025-02-22 13:18:43 +0000

    Live Camera Feed

--- camera/main.py
+++ camera/main.py
@@ -1,3 +1,26 @@
 import cv2
 
-print(cv2.__version__)
+cap = cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+cap.set(3, 640)  # adjust width
+cap.set(4, 480)  # adjust height
+
+while True:
+    success, img = cap.read()
+    
+    if not success:
+        print("Failed to capture image")
+        break
+    
+    cv2.imshow("Live Feed", img)  # Changed window name to "Live Feed"
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # quit when 'q' is pressed
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
