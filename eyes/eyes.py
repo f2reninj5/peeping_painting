200def3c70d0e6da9c69791b8b9fa0539e89d154
fn <64966099+f2reninj5@users.noreply.github.com>
2025-02-22 20:43:00 +0000

    Code tidy up

--- eyes/eyes.py
+++ eyes/eyes.py
@@ -4,210 +4,12 @@
 # an embarrassing number of globals in the frame() function and stuff.
 # Needed to get SOMETHING working, can focus on improvements next.
 # Requires adafruit-blinka (CircuitPython APIs for Python on big hardware)
+from camera.track import setup_camera, track
+import numpy as np
+from setup import *
 
 eye_center_x = 0
 eye_center_y = 0
-
-import argparse
-import math
-import pi3d
-import random
-import threading
-import time
-import board
-import digitalio
-from svg.path import Path, parse_path
-from xml.dom.minidom import parse
-from gfxutil import *
-from snake_eyes_bonnet import SnakeEyesBonnet
-from camera.track import setup_camera, track
-import numpy as np
-
-# INPUT CONFIG for eye motion ----------------------------------------------
-# ANALOG INPUTS REQUIRE SNAKE EYES BONNET
-
-PUPIL_IN        = -1    # Analog input for pupil control (-1 = auto)
-PUPIL_SMOOTH    = 16    # If > 0, filter input from PUPIL_IN
-PUPIL_MIN       = 0.0   # Lower analog range from PUPIL_IN
-PUPIL_MAX       = 1.0   # Upper "
-
-# Load SVG file, extract paths & convert to point lists --------------------
-
-dom               = parse("graphics/eye.svg")
-vb                = get_view_box(dom)
-pupilMinPts       = get_points(dom, "pupilMin"      , 32, True , True )
-pupilMaxPts       = get_points(dom, "pupilMax"      , 32, True , True )
-irisPts           = get_points(dom, "iris"          , 32, True , True )
-scleraFrontPts    = get_points(dom, "scleraFront"   ,  0, False, False)
-scleraBackPts     = get_points(dom, "scleraBack"    ,  0, False, False)
-
-
-# Set up display and initialize pi3d ---------------------------------------
-
-DISPLAY = pi3d.Display.create(samples=4)
-DISPLAY.set_background(0, 0, 0, 1) # r,g,b,alpha
-
-# eyeRadius is the size, in pixels, at which the whole eye will be rendered
-# onscreen.  eyePosition, also pixels, is the offset (left or right) from
-# the center point of the screen to the center of each eye.  This geometry
-# is explained more in-depth in fbx2.c.
-eyePosition = DISPLAY.width / 4
-eyeRadius   = 128  # Default; use 240 for IPS screens
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--radius", type=int)
-args, _ = parser.parse_known_args()
-if args.radius:
-	eyeRadius = args.radius
-
-
-# A 2D camera is used, mostly to allow for pixel-accurate eye placement,
-# but also because perspective isn't really helpful or needed here, and
-# also this allows eyelids to be handled somewhat easily as 2D planes.
-# Line of sight is down Z axis, allowing conventional X/Y cartesion
-# coords for 2D positions.
-cam    = pi3d.Camera(is_3d=False, at=(0,0,0), eye=(0,0,-1000))
-shader = pi3d.Shader("uv_light")
-light  = pi3d.Light(lightpos=(0, -500, -500), lightamb=(0.2, 0.2, 0.2))
-
-
-# Load texture maps --------------------------------------------------------
-
-irisMap   = pi3d.Texture("graphics/iris.jpg"  , mipmap=False,
-              filter=pi3d.constants.GL_LINEAR)
-scleraMap = pi3d.Texture("graphics/sclera.png", mipmap=False,
-              filter=pi3d.constants.GL_LINEAR, blend=True)
-# U/V map may be useful for debugging texture placement; not normally used
-#uvMap     = pi3d.Texture("graphics/uv.png"    , mipmap=False,
-#              filter=pi3d.constants.GL_LINEAR, blend=False, m_repeat=True)
-
-
-# Initialize static geometry -----------------------------------------------
-
-# Transform point lists to eye dimensions
-scale_points(pupilMinPts      , vb, eyeRadius)
-scale_points(pupilMaxPts      , vb, eyeRadius)
-scale_points(irisPts          , vb, eyeRadius)
-scale_points(scleraFrontPts   , vb, eyeRadius)
-scale_points(scleraBackPts    , vb, eyeRadius)
-
-# Regenerating flexible object geometry (such as eyelids during blinks, or
-# iris during pupil dilation) is CPU intensive, can noticably slow things
-# down, especially on single-core boards.  To reduce this load somewhat,
-# determine a size change threshold below which regeneration will not occur;
-# roughly equal to 1/4 pixel, since 4x4 area sampling is used.
-
-# Determine change in pupil size to trigger iris geometry regen
-irisRegenThreshold = 0.0
-a = points_bounds(pupilMinPts) # Bounds of pupil at min size (in pixels)
-b = points_bounds(pupilMaxPts) # " at max size
-maxDist = max(abs(a[0] - b[0]), abs(a[1] - b[1]), # Determine distance of max
-              abs(a[2] - b[2]), abs(a[3] - b[3])) # variance around each edge
-# maxDist is motion range in pixels as pupil scales between 0.0 and 1.0.
-# 1.0 / maxDist is one pixel's worth of scale range.  Need 1/4 that...
-if maxDist > 0: irisRegenThreshold = 0.25 / maxDist
-
-# Generate initial iris meshes; vertex elements will get replaced on
-# a per-frame basis in the main loop, this just sets up textures, etc.
-rightIris = mesh_init((32, 4), (0, 0.5 / irisMap.iy), True, False)
-rightIris.set_textures([irisMap])
-rightIris.set_shader(shader)
-# Left iris map U value is offset by 0.5; effectively a 180 degree
-# rotation, so it's less obvious that the same texture is in use on both.
-leftIris = mesh_init((32, 4), (0.5, 0.5 / irisMap.iy), True, False)
-leftIris.set_textures([irisMap])
-leftIris.set_shader(shader)
-irisZ = zangle(irisPts, eyeRadius)[0] * 0.99 # Get iris Z depth, for later
-
-# Generate scleras for each eye...start with a 2D shape for lathing...
-angle1 = zangle(scleraFrontPts, eyeRadius)[1] # Sclera front angle
-angle2 = zangle(scleraBackPts , eyeRadius)[1] # " back angle
-aRange = 180 - angle1 - angle2
-pts    = []
-
-# ADD EXTRA INITIAL POINT because of some weird behavior with Pi3D and
-# VideoCore VI with the Lathed shapes we make later. This adds a *tiny*
-# ring of extra polygons that simply disappear on screen. It's not
-# necessary on VC4, but not harmful either, so we just do it rather
-# than try to be all clever.
-ca, sa = pi3d.Utility.from_polar((90 - angle1) + aRange * 0.0001)
-pts.append((ca * eyeRadius, sa * eyeRadius))
-
-for i in range(24):
-	ca, sa = pi3d.Utility.from_polar((90 - angle1) - aRange * i / 23)
-	pts.append((ca * eyeRadius, sa * eyeRadius))
-
-# Scleras are generated independently (object isn't re-used) so each
-# may have a different image map (heterochromia, corneal scar, or the
-# same image map can be offset on one so the repetition isn't obvious).
-leftEye = pi3d.Lathe(path=pts, sides=64)
-leftEye.set_textures([scleraMap])
-leftEye.set_shader(shader)
-re_axis(leftEye, 0)
-rightEye = pi3d.Lathe(path=pts, sides=64)
-rightEye.set_textures([scleraMap])
-rightEye.set_shader(shader)
-re_axis(rightEye, 0.5) # Image map offset = 180 degree rotation
-
-
-# Init global stuff --------------------------------------------------------
-
-mykeys = pi3d.Keyboard() # For capturing key presses
-
-startX       = random.uniform(-30.0, 30.0)
-n            = math.sqrt(900.0 - startX * startX)
-startY       = random.uniform(-n, n)
-destX        = startX
-destY        = startY
-curX         = startX
-curY         = startY
-moveDuration = random.uniform(0.075, 0.175)
-holdDuration = random.uniform(0.1, 1.1)
-startTime    = 0.0
-isMoving     = False
-
-startXR      = random.uniform(-30.0, 30.0)
-n            = math.sqrt(900.0 - startX * startX)
-startYR      = random.uniform(-n, n)
-destXR       = startXR
-destYR       = startYR
-curXR        = startXR
-curYR        = startYR
-moveDurationR = random.uniform(0.075, 0.175)
-holdDurationR = random.uniform(0.1, 1.1)
-startTimeR    = 0.0
-isMovingR     = False
-
-frames        = 0
-beginningTime = time.time()
-
-rightEye.positionX(-eyePosition)
-rightIris.positionX(-eyePosition)
-
-leftEye.positionX(eyePosition)
-leftIris.positionX(eyePosition)
-
-currentPupilScale       =  0.5
-prevPupilScale          = -1.0 # Force regen on first frame
-
-luRegen = True
-llRegen = True
-ruRegen = True
-rlRegen = True
-
-timeOfLastBlink = 0.0
-timeToNextBlink = 1.0
-# These are per-eye (left, right) to allow winking:
-blinkStateLeft      = 0 # NOBLINK
-blinkStateRight     = 0
-blinkDurationLeft   = 0.1
-blinkDurationRight  = 0.1
-blinkStartTimeLeft  = 0
-blinkStartTimeRight = 0
-
-trackingPos = 0.3
-trackingPosR = 0.3
-
 setup_camera()
 
 # Generate one frame of imagery
@@ -223,11 +25,6 @@ def frame(p):
 	global leftEye, rightEye
 	global prevPupilScale
 	global irisRegenThreshold
-	global luRegen, llRegen, ruRegen, rlRegen
-	global timeOfLastBlink, timeToNextBlink
-	global blinkStateLeft, blinkStateRight
-	global blinkDurationLeft, blinkDurationRight
-	global blinkStartTimeLeft, blinkStartTimeRight
 	global trackingPos
 	global trackingPosR
 
@@ -261,7 +58,7 @@ def frame(p):
 		if dt >= holdDuration:
 			xx, yy = track()
 			destX        = np.interp(xx, [0, 1280], [-30, 30])
-			destY        = np.interp(yy, [0, 720], [-30, 30])
+			destY        = -np.interp(yy, [0, 720], [-30, 30])
 			moveDuration = random.uniform(0.075, 0.175)
 			startTime    = now
 			isMoving     = True
@@ -294,12 +91,6 @@ def frame(p):
 	leftEye.rotateToX(curY)
 	leftEye.rotateToY(curX + convergence)
 	leftEye.draw()
-
-	k = mykeys.read()
-	if k==27:
-		mykeys.close()
-		DISPLAY.stop()
-		exit(0)
 
 
 def split( # Recursive simulated pupil response when no analog sensor
